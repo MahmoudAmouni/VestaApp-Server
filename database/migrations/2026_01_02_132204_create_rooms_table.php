f6a1e307543f06539814d215318a6cf36c326092
Mahmoud Amouni <mhmdamoun0@gmail.com>
2026-01-02 15:22:59 +0200

    created rooms table

---
+++ database/migrations/2026_01_02_132204_create_rooms_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('home_id')->index();
+            $table->unsignedBigInteger('room_name_id')->index();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
