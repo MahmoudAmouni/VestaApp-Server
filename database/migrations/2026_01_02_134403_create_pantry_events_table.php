a7c1964753dfa5ced4ae102814ff015838c5ddf3
Mahmoud Amouni <mhmdamoun0@gmail.com>
2026-01-02 15:45:19 +0200

    created pantry_events table

---
+++ database/migrations/2026_01_02_134403_create_pantry_events_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pantry_events', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->unsignedBigInteger('home_id')->index();
+            $table->unsignedBigInteger('pantry_item_id')->nullable()->index();
+
+            $table->string('event_type', 20);
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pantry_events');
+    }
+};
