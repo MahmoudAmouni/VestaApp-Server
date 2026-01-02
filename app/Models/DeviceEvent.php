93b5592e0e04e20bedb14ad9667a69e8d89f9ea4
Mahmoud Amouni <mhmdamoun0@gmail.com>
2026-01-02 22:25:11 +0200

    created DeviceEvent model

---
+++ app/Models/DeviceEvent.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class DeviceEvent extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'device_id',
+        'action',
+    ];
+
+    public function device(): BelongsTo
+    {
+        return $this->belongsTo(Device::class, 'device_id');
+    }
+}
