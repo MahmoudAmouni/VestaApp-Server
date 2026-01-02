b93ba989372a962aba1204468257345917c64f9d
Mahmoud Amouni <mhmdamoun0@gmail.com>
2026-01-02 19:26:21 +0200

    updated users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,28 +12,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('diet_id')->nullable()->index();
+            $table->unsignedBigInteger('allergy_id')->nullable()->index();
+
+            $table->string('email', 255)->unique();
+            $table->string('name', 255);
+            $table->string('avatar_url', 1024)->nullable();
+            $table->string('phone', 50)->nullable();
+
             $table->timestamps();
-        });
-
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
+            $table->softDeletes();
         });
     }
 
