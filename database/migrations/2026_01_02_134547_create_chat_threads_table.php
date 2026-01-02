b43feceb03aa35669d581603e361dc96e88f93f6
Mahmoud Amouni <mhmdamoun0@gmail.com>
2026-01-02 15:46:30 +0200

    created chat_threads table

---
+++ database/migrations/2026_01_02_134547_create_chat_threads_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chat_threads', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->unsignedBigInteger('user_id')->index();
+            $table->unsignedBigInteger('home_id')->nullable()->index();
+
+            $table->string('title', 255)->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chat_threads');
+    }
+};
