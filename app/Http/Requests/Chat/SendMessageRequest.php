fca2432fc5ab613685a6598f47540765679d7a41
Mahmoud Amouni <mhmdamoun0@gmail.com>
2026-01-05 09:27:57 +0200

    created SendMessageRequest

---
+++ app/Http/Requests/Chat/SendMessageRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Requests\Chat;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SendMessageRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'message' => ['required', 'string', 'max:8000'],
+        ];
+    }
+}
