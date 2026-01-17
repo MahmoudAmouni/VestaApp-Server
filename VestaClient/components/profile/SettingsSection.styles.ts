fae44afd25ebf7b1a471081091eb8a77fe175c02
Mahmoud Amouni <mhmdamoun0@gmail.com>
2026-01-17 19:58:50 +0200

    Client -> Created SettingsSection Style

---
+++ VestaClient/components/profile/SettingsSection.styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const settingsSectionStyles = StyleSheet.create({
+  wrap: {
+    gap: 10,
+  },
+  title: {
+    fontSize: 15,
+    fontWeight: "900",
+    paddingHorizontal: 2,
+  },
+  card: {
+    overflow: "hidden",
+  },
+});
