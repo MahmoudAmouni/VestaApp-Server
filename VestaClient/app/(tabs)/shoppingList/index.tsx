a06ff48046cdae5d7baaf6970ad35faa3fc9ca39
Mahmoud Amouni <mhmdamoun0@gmail.com>
2026-01-17 19:15:53 +0200

    Client -> Created (tab)/shoppingList

---
+++ VestaClient/app/(tabs)/shoppingList/index.tsx
@@ -0,0 +1,8 @@
+import ShoppingListScreen from '@/components/ShoppingList/ShoppingListScreen'
+import React from 'react'
+
+export default function shoppingList() {
+  return (
+    <ShoppingListScreen />
+  )
+}
