0fac51a31018abde0334b4435134d7d54d4d24ad
Mahmoud Amouni <mhmdamoun0@gmail.com>
2026-01-17 20:53:05 +0200

    Client -> Created VestaBackdrop Component

---
+++ VestaClient/components/Vesta/VestaBackdrop.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import { vestaBackdropStyles as styles } from './VestaBackdrop.styles';
+
+interface VestaBackdropProps {
+  onPress: () => void;
+  children: React.ReactNode;
+}
+
+export default function VestaBackdrop(props: VestaBackdropProps) {
+  const { onPress, children } = props;
+
+  return (
+    <TouchableOpacity
+      style={styles.backdrop}
+      activeOpacity={1}
+      onPress={onPress}
+      accessibilityRole="button"
+      accessibilityLabel="Close voice assistant"
+    >
+      {children}
+    </TouchableOpacity>
+  );
+}
