dd5718ada0b45f7b93e1b2ead602d9d8ea82dcb7
Mahmoud Amouni <mhmdamoun0@gmail.com>
2026-01-17 19:34:58 +0200

    Client -> Created PantryFilter Style

---
+++ VestaClient/components/Pantry/PantryFilterRow.styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export const pantryFilterRowStyles = StyleSheet.create({
+  row: {
+    gap: 8,
+    paddingRight: 8,
+  },
+  pill: {
+    borderWidth: 1,
+    borderRadius: 999,
+    paddingHorizontal: 12,
+    paddingVertical: 7,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  pillText: {
+    fontSize: 12,
+    fontWeight: "900",
+  },
+});
