ec0f2ae19c40c1c156cfac6463cfe55425c151a0
Mahmoud Amouni <mhmdamoun0@gmail.com>
2026-01-17 20:48:25 +0200

    Client -> Created ShoppingHero Style

---
+++ VestaClient/components/ShoppingList/ShoppingHero.styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const shoppingHeroStyles = StyleSheet.create({
+  btnRow: {
+    flexDirection: "row",
+    gap: 10,
+    marginTop: 10,
+  },
+  heroBtn: {
+    flex: 1,
+    height: 40,
+    borderRadius: 14,
+  },
+});
