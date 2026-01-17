61f7eacc44b5d0c46e199d86ad1fa581d36e1b43
Mahmoud Amouni <mhmdamoun0@gmail.com>
2026-01-17 19:20:32 +0200

    Client -> Created bottomNav

---
+++ VestaClient/components/ui/BottomNav.styles.ts
@@ -0,0 +1,32 @@
+import { Platform, StyleSheet } from "react-native";
+
+export const bottomstyles = StyleSheet.create({
+  bottomNav: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    bottom: 0,
+    borderTopWidth: 1,
+    paddingTop: 4,
+    paddingBottom: Platform.OS === "ios" ? 22 : 8,
+    paddingHorizontal: 8,
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  navItem: {
+    alignItems: "center",
+    gap: 6,
+    minWidth: 56,
+  },
+  navIconWrap: {
+    width: 60,
+    height: 50,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  navText: {
+    fontSize: 11,
+    fontWeight: "800",
+  },
+});
