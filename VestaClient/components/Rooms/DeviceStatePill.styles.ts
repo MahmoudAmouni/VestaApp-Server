2cf36feaed2071d251bf5e60f76971f39a4ebc93
Mahmoud Amouni <mhmdamoun0@gmail.com>
2026-01-17 20:19:15 +0200

    Client -> Created DeviceStatePil Style

---
+++ VestaClient/components/Rooms/DeviceStatePill.styles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+export const deviceStatePillStyles = StyleSheet.create({
+  pill: {
+    minWidth: 56,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
