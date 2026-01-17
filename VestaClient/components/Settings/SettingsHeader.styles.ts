f477e966fb5063d3c25089d37cbe38ecc8fc792d
Mahmoud Amouni <mhmdamoun0@gmail.com>
2026-01-17 20:39:30 +0200

    Client -> Created SettingsHeader Style

---
+++ VestaClient/components/Settings/SettingsHeader.styles.ts
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+export const settingsHeaderStyles = StyleSheet.create({
+  wrap: {
+    paddingHorizontal: 16,
+    paddingTop: 10,
+    paddingBottom: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+  },
+  backBtn: {
+    width: 34,
+    height: 34,
+    borderRadius: 12,
+    borderWidth: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    flex: 1,
+    fontSize: 15,
+    fontWeight: "900",
+  },
+  rightSpacer: {
+    width: 34,
+    height: 34,
+  },
+});
