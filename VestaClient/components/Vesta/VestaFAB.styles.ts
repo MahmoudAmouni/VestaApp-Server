f3f44cb3ddf9c845123a3cf967de9f827f77c0ef
Mahmoud Amouni <mhmdamoun0@gmail.com>
2026-01-17 20:52:42 +0200

    Client -> Created VestaFAB Style

---
+++ VestaClient/components/Vesta/VestaFAB.styles.ts
@@ -0,0 +1,25 @@
+import { VESTA_CONFIG } from '@/constants/vesta.constants';
+import { StyleSheet } from 'react-native';
+
+export const vestaFABStyles = StyleSheet.create({
+  fab: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    backgroundColor: VESTA_CONFIG.COLORS.primary,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  fabExpanded: {
+    backgroundColor: '#FF4444', // Red for stop
+    transform: [{ scale: 1.1 }],
+  },
+});
