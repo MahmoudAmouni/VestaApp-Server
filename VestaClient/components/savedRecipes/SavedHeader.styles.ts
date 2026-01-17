38e4957dbf41232edd6d0db2dc59ee51758ac0f2
Mahmoud Amouni <mhmdamoun0@gmail.com>
2026-01-17 20:32:19 +0200

    Client -> Created SavedHeader Style

---
+++ VestaClient/components/savedRecipes/SavedHeader.styles.ts
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+
+export const savedHeaderStyles = StyleSheet.create({
+  wrap: {
+    paddingHorizontal: 16,
+    paddingBottom: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+  },
+  backBtn: {
+    width: 34,
+    height: 34,
+    borderRadius: 12,
+    borderWidth: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    flex: 1,
+    fontSize: 15,
+    fontWeight: "900",
+  },
+  rightSpacer: {
+    width: 34,
+    height: 34,
+  },
+});
