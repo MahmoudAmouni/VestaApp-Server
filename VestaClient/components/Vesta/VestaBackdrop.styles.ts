00853f7e92b45ef1fa6ae080b9695613648b6649
Mahmoud Amouni <mhmdamoun0@gmail.com>
2026-01-17 20:53:22 +0200

    Client -> Created VestaBackdrop Style

---
+++ VestaClient/components/Vesta/VestaBackdrop.styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export const vestaBackdropStyles = StyleSheet.create({
+  backdrop: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'rgba(0,0,0,0.6)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
