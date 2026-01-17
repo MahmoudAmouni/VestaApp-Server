8f5ab04fb551fd2e7a3808f4c4ef7b0181e91162
Mahmoud Amouni <mhmdamoun0@gmail.com>
2026-01-17 20:47:25 +0200

    Client -> Created ItemsSectionStyle

---
+++ VestaClient/components/ShoppingList/ItemsSection.styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export const itemsSectionStyles = StyleSheet.create({
+  card: { overflow: "hidden" },
+  clearBtn: {
+    width: "100%",
+    borderRadius: 14,
+    height: 46,
+  },
+});
