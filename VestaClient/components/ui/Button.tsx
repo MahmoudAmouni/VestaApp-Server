62a7cb2be1a6e0c70af58d7f6f9626d984861f15
Mahmoud Amouni <mhmdamoun0@gmail.com>
2026-01-17 19:21:13 +0200

    Client -> Created button

---
+++ VestaClient/components/ui/Button.tsx
@@ -0,0 +1,51 @@
+import { theme } from "@/constants/theme";
+import { Pressable, StyleSheet, Text, ViewStyle } from "react-native";
+
+type ButtonVariant = "primary" | "secondary";
+
+export default function Button(props: {
+  variant: ButtonVariant;
+  label: string;
+  onPress: () => void;
+  disabled?:boolean
+  style?: ViewStyle; 
+}) {
+  const { disabled } = props;
+
+  const bg = props.variant === "primary" ? theme.primary : theme.surface2;
+  const border = props.variant === "primary" ? theme.primary : theme.border;
+
+  return (
+    <Pressable
+      onPress={props.onPress}
+      style={({ pressed }) => [
+        styles.btn,
+        props.style,
+        {
+          backgroundColor: bg,
+          borderColor: border,
+          opacity: pressed ? 0.9 : 1,
+        },
+      ]}
+      accessibilityRole="button"
+      disabled={disabled}
+    >
+      <Text style={[styles.btnText, { color: "#fff" }]}>{props.label}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  btn: {
+    borderWidth: 1,
+    borderRadius: 14,
+    paddingVertical: 12,
+    paddingHorizontal: 14,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  btnText: {
+    fontSize: 13,
+    fontWeight: "900",
+  },
+});
