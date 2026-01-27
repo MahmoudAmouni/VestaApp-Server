5dc823db88e84329970767c9e55a89356ff07b07
Mahmoud Amouni <mhmdamoun0@gmail.com>
2026-01-27 20:41:53 +0200

    Server -> Created config database

--- VestaAppServer/Go/main.go
+++ VestaAppServer/Go/main.go
@@ -16,7 +16,7 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:3000"}, // Allow frontend origins
+		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:3000"}, 
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"},
 		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type"},
 		ExposeHeaders:    []string{"Content-Length"},
